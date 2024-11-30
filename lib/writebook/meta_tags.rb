71e22aac2260ab9290467abe83bd740069a445d1
Lucian Ghinda <lucianghinda@users.noreply.github.com>
2024-11-30 08:35:05 +0200

    Initial commit - generated with bundle gem writebook-meta_tags

---
+++ lib/writebook/meta_tags.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "meta_tags/version"
+
+module Writebook
+  module MetaTags
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/writebook/meta_tags/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Writebook
+  module MetaTags
+    VERSION = "0.1.0"
+  end
+end
